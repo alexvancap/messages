c2d3b0b1aa41c198912a132be35259bd84462c60
alexvancap <alexvancap@gmail.com>
2020-05-19 18:23:19 +0200

    finished basic messages styling

--- client/src/components/messages/MessageCard.js
+++ client/src/components/messages/MessageCard.js
@@ -4,7 +4,10 @@ import { Container } from 'semantic-ui-react'
 export const MessageCard = () => {
     return (
         <Container className='message-card'>
-
+            <div className='message-card-img' />
+            <p className='message-card-text'>hallo</p>
+            <p className='message-card-username'>alexvancap</p>
+            <p className='message-card-date'>13/09/1998</p>
         </Container>
     )
 }
--- client/src/components/messages/MessageOverview.js
+++ client/src/components/messages/MessageOverview.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Container, Header } from 'semantic-ui-react'
-import { NewMessageBtn } from './NewMessageBtn'
+import { NewMessageBtn, MessageCard } from './'
 
 export const MessageOverview = () => {
     return (
         <Container id='message-overview'>
-            <Container id='prev-msgs-header'>
-                <Header style={{marginTop: '5%'}}>Previous Messages</Header>
+            <Container id='message-header'>
+                <Header id='msgs-header-text'>Previous Messages</Header>
                 <Container id='messages-border-btm'/>
                 <Container id='message-cards-cont'>
-
+                    <MessageCard />
+                    <MessageCard />
                 </Container>
                 <NewMessageBtn />
             </Container>
