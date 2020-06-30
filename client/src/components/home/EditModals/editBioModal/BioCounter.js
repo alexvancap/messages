0948dedca776c8896d1d183b13303ac246779683
alexvancap <alexvancap@gmail.com>
2020-06-30 12:33:37 +0200

    fixed empty bio error

--- client/src/components/home/EditModals/editBioModal/BioCounter.js
+++ client/src/components/home/EditModals/editBioModal/BioCounter.js
@@ -5,18 +5,19 @@ import { Header } from 'semantic-ui-react';
 export const BioCounter = () => {
   const bio = useSelector(state => state.user.bio);
   const [progress, setProgress] = useState(0);
-  const [disabled, setDisabled] = useState(false)
+  const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
-    setProgress(bio.length)
-    if(progress > 140) setDisabled(true)
+    if(!bio) return;
+    else if(progress > 140) setDisabled(true);
+    else setProgress(bio.length);
   }, [bio])
   return (
     <Header 
       inverted 
       disabled={disabled} 
       as='h3' 
-      style={{textAlign: 'center', color: bio.length === 140 ? 'red' : ''}}
+      style={{textAlign: 'center', color: progress === 140 ? 'red' : ''}}
     >
       {progress}/140
     </Header>
